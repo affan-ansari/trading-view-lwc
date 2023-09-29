bf5181b188e035f4b781115670eeb03dcae13f45
Affan Arif <affanarif74@gmail.com>
2023-09-30 00:43:24 +0500

    Commented math.pow ftn

--- src/Services/fetchData.js
+++ src/Services/fetchData.js
@@ -21,24 +21,23 @@ export const fetchData = (contract, decimals, handleEvent) => {
 };
 
 const calculatePrice = ({ dec0, dec1 }, txData) => {
-  const { amount0In, amount0Out, amount1In, amount1Out } = txData.returnValues;
-  let txType, price;
-  if (amount0In > 0) {
-    txType = "sell";
-    const amount1_out = Number(amount1Out) / Math.pow(10, Number(dec1));
-    const amount0_in = Number(amount0In) / Math.pow(10, Number(dec0));
-
-    price = amount1_out / amount0_in;
-  }
-  // diff_percentage = ((price - self.last_price)/price)*100
-  // self.last_price = price
-  else {
-    txType = "buy";
-    const amount1_in = Number(amount1In) / Math.pow(10, Number(dec1));
-    const amount0_out = Number(amount0Out) / Math.pow(10, Number(dec0));
-    price = amount1_in / amount0_out;
-  }
-  //     diff_percentage = ((price - self.last_price)/price)*100
-  //     self.last_price = price
-  return { txType, price };
+  return { txType: "sell", price: 0.00052 };
 };
+// const calculatePrice = ({ dec0, dec1 }, txData) => {
+//   const { amount0In, amount0Out, amount1In, amount1Out } = txData.returnValues;
+//   let txType, price;
+//   if (amount0In > 0) {
+//     txType = "sell";
+//     const amount1_out = Number(amount1Out) / Math.pow(10, Number(dec1));
+//     const amount0_in = Number(amount0In) / Math.pow(10, Number(dec0));
+
+//     price = amount1_out / amount0_in;
+//   }
+//   else {
+//     txType = "buy";
+//     const amount1_in = Number(amount1In) / Math.pow(10, Number(dec1));
+//     const amount0_out = Number(amount0Out) / Math.pow(10, Number(dec0));
+//     price = amount1_in / amount0_out;
+//   }
+//   return { txType, price };
+// };

6dbb2c77a7adbf9b17cfd98dec3eaebad83976b9
Affan Arif <affanarif74@gmail.com>
2023-10-03 00:41:27 +0500

    removed fixed infuraId and pairAddress

--- src/reducers/credentials/credentialsSlice.js
+++ src/reducers/credentials/credentialsSlice.js
@@ -2,8 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   credentials: {
-    infuraId: "f370c580e10c471cbe322f4674cd06c8",
-    pairAddress: "0x2cC846fFf0b08FB3bFfaD71f53a60B4b6E6d6482",
+    infuraId: "",
+    pairAddress: "",
   },
 };
 
